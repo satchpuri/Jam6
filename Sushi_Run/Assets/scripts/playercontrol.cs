a64306052e43db4386b59c90ea27c8180c2b82e6
MYLAPTOP\Mark <markobeldobel@yahoo.com>
2018-03-14 14:46:35 -0400

    First Commit with Unity Stuff.

---
+++ Sushi_Run/Assets/scripts/playercontrol.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playercontrol : MonoBehaviour {
+
+	[SerializeField] private float speed;
+	[SerializeField] private float limitRight;
+	[SerializeField] private float limitLeft;
+	[SerializeField] private int score;
+
+	//how many nsushi rolls the player is holding
+	public int holding;
+	//the maximum number of sushi rolls the player can hold
+	public int maxHold;
+
+	[SerializeField] private Text scoreTextBox;
+
+	private string scoreText = "Score: ";
+
+	// Use this for initialization
+	void Start () {
+		holding = 0;
+		maxHold = 2;
+
+		speed = 10.0f;
+		limitRight = 10.0f;
+		limitLeft = -12.75f;
+		score = 0;
+
+		scoreTextBox.text = "Score: " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		scoreTextBox.text = "Score: " + score;
+
+		if (Input.GetAxis("Horizontal") > 0 && transform.position.x < limitRight) 
+		{
+			transform.position += new Vector3(speed, 0.0f, 0.0f) * Time.deltaTime;
+		}
+		else if (Input.GetAxis("Horizontal") < 0 && transform.position.x > limitLeft) 
+		{
+			transform.position += new Vector3(-speed, 0.0f, 0.0f) * Time.deltaTime;
+		}
+	}
+
+	public int GetHolding()
+	{
+		return holding;
+	}
+
+	public void SetHolding(int value)
+	{
+		holding = value;
+	}
+
+	public int GetMaxHold()
+	{
+		return maxHold;
+	}
+}
---
+++ Sushi_Run/Assets/scripts/sushicollect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sushicollect : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+	[SerializeField] private playercontrol plyctrl;
+	//the bounds within you must be near the sushi to pick it up
+	[SerializeField] private float distanceOffset;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		plyctrl = player.GetComponent<playercontrol>();
+
+		distanceOffset = 1.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//try to grab a sushi roll
+		if (Input.GetAxis("Jump") > 0) 
+		{
+			print ("Clicking!");
+			//within range of the sushi roll
+			if (player.transform.position.x + distanceOffset > transform.position.x && player.transform.position.x - distanceOffset < transform.position.x) 
+			{
+				//check if the player can hold more sushi
+				if (plyctrl.GetHolding() < plyctrl.GetMaxHold()) 
+				{
+					plyctrl.SetHolding(plyctrl.GetHolding () + 1);
+					print ("Deleted!");
+					Destroy(gameObject);
+				}
+			}
+		}
+
+	}
+}
