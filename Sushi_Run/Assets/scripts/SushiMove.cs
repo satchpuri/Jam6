1732fd3fe032669068e32ab1a1be9f6272388e90
Kaustabh Dutta <kaustabh.dutta@gmail.com>
2018-03-26 18:41:09 -0400

    Trying to work on the sushi generator. Any help is appreciated.

---
+++ Sushi_Run/Assets/scripts/SushiMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SushiMove : MonoBehaviour {
+
+    public Transform StartingPoint;
+    public Transform EndingPoint;
+    public static float Speed;
+
+	void Start () {
+        Speed = 1.0f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+            transform.position += new Vector3(Speed * Time.deltaTime, 0, 0);
+	}
+
+
+}
